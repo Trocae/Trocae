3a282a22f1f608092948f4e63f0de36dd538e8ee
Trocaê <ianfelixbelial@gmail.com>
2026-08-18 17:31:30 -0300

    Add Firebase configuration and constants

---
+++ js/config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY_AQUI",
+  authDomain: "trocae-f94e1.firebaseapp.com",
+  projectId: "trocae-f94e1",
+  storageBucket: "trocae-f94e1.appspot.com",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID",
+};
+
+export const VERIFICATION_URL = "https://trocae-f94e1.firebaseapp.com";
+
+export const LOCAL_BLOCKED_USERS = [];
